544678f0722e24f50232928809b661a1fd48eae5
dmytrokain <dimikowowik@gmail.com>
2020-06-28 16:16:34 +0300

    Add validation for Subject model.

--- src/models/subject.js
+++ src/models/subject.js
@@ -4,7 +4,18 @@ module.exports = (sequelize, type) => sequelize.define('subject', {
     primaryKey: true,
     autoIncrement: true,
   },
-  name: type.STRING,
-  theme: type.STRING,
-  scheduleNumber: type.INTEGER,
+  name: {
+    type: type.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { msg: 'name of subject is required' },
+    },
+  },
+  scheduleNumber: {
+    type: type.INTEGER,
+    allowNull: false,
+    validate: {
+      notNull: { msg: 'schedule number of subject is required' },
+    },
+  },
 });
