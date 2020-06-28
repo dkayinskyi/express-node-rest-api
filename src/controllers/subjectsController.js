0a5f9af76cc708bcaf543f300a728e2b5ad105b4
dmytrokain <dimikowowik@gmail.com>
2020-06-28 17:23:26 +0300

    Create endpoints for GET, PATCH and Delete the subject.

--- src/controllers/subjectsController.js
+++ src/controllers/subjectsController.js
@@ -4,6 +4,29 @@ exports.subjectsList = (req, res) => {
   Subject.findAll().then((user) => res.json(user));
 };
 
+exports.getSubject = (req, res) => {
+  Subject.findOne({
+    where: { id: req.params.id },
+  }).then((user) => res.json(user));
+};
+
+exports.deleteSubject = (req, res) => {
+  Subject.destroy({
+    where: { id: req.params.id },
+  }).then((queryResult) => {
+    let message = 'Subject was deleted';
+    let status = 200;
+
+    if (queryResult !== 1) {
+      status = 404;
+      message = 'Requested subject not exists';
+    }
+
+    res.status(status);
+    res.json({ message });
+  });
+};
+
 exports.createSubject = (req, res) => {
   Subject.create(req.body)
     .then((subject) => res.json(subject))
@@ -12,4 +35,10 @@ exports.createSubject = (req, res) => {
       res.status(400);
       res.json({ errors });
     });
+};
+
+exports.updateSubject = (req, res) => {
+  Subject.update(req.body, {
+    where: { id: req.params.id },
+  }).then((user) => res.json(user));
 };
--- src/routes/subject.js
+++ src/routes/subject.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const subjectsController = require('../controllers/subjectsController');
 
 router.get('/', subjectsController.subjectsList);
+router.get('/:id', subjectsController.getSubject);
+
 router.post('/', subjectsController.createSubject);
+router.patch('/:id', subjectsController.updateSubject);
+
+router.delete('/:id', subjectsController.deleteSubject);
 
 module.exports = router;
